848eddd90832df81b59097d230c70cb836de7704
wowns1484 <wowns1484@naver.com>
2022-06-24 14:08:28 +0900

    [update] code

--- util/data_generator_AE.py
+++ util/data_generator_AE.py
@@ -8,6 +8,8 @@ import numpy as np
 from sklearn.preprocessing import Normalizer, MinMaxScaler, RobustScaler, StandardScaler
 from sklearn.pipeline import Pipeline
 from util.data_management import concate_data, stack_data
+import math
+import random
 
 class TrainMode(enum.Enum):
     TRAIN = 'Train'
@@ -15,7 +17,7 @@ class TrainMode(enum.Enum):
     TEST = 'Test'
 
 class DataGenerator(Sequence):
-    def __init__(self, dataset_path : str, train_mode : str, batch_size : int, split : tuple, is_train : bool, is_cache : bool, is_normalize : bool, is_lstm : bool) -> None:
+    def __init__(self, dataset_path : str, train_mode : str, batch_size : int, split : tuple, is_train : bool, is_cache : bool, is_normalize : bool, is_lstm : bool, is_predict = None) -> None:
         super().__init__()
         self.AUTOTUNE = tf.data.experimental.AUTOTUNE
         self.dataset_path = dataset_path
@@ -27,6 +29,7 @@ class DataGenerator(Sequence):
         self.is_normalize = is_normalize
         self.is_lstm = is_lstm
         self.is_adjust_fit = True
+        self.is_predict = is_predict
         self.cache = {}
 
         if self.is_normalize:
@@ -63,40 +66,7 @@ class DataGenerator(Sequence):
             self.data_paths = list(zip(data_paths_0, data_paths_1, data_paths_2, data_paths_3))
 
     def __len__(self):
-        return len(self.data_paths) // self.batch_size
-        
-    # def __call__(self) -> tf.data.Dataset:
-    #     dataset = []
-        
-    #     for path_0, path_1, path_2, path_3 in self.data_paths:
-    #         data_0 = pd.read_csv(path_0, encoding='unicode_escape', delimiter='\t', header=None)
-    #         data_1 = pd.read_csv(path_1, encoding='unicode_escape', delimiter='\t', header=None)
-    #         data_2 = pd.read_csv(path_2, encoding='unicode_escape', delimiter='\t', header=None)
-    #         data_3 = pd.read_csv(path_3, encoding='unicode_escape', delimiter='\t', header=None)
-
-    #         data_0 = np.array(data_0[0].values.tolist()[1:])
-    #         data_1 = np.array(data_1[0].values.tolist()[1:])
-    #         data_2 = np.array(data_2[0].values.tolist()[1:])
-    #         data_3 = np.array(data_3[0].values.tolist()[1:])
-
-    #         data_0 = np.mean(np.abs(data_0), axis=0).reshape(-1, 1)
-    #         data_1 = np.mean(np.abs(data_1), axis=0).reshape(-1, 1)
-    #         data_2 = np.mean(np.abs(data_2), axis=0).reshape(-1, 1)
-    #         data_3 = np.mean(np.abs(data_3), axis=0).reshape(-1, 1)
-            
-    #         data = concate_data([data_0, data_1, data_2, data_3], axis=1)
-    #         dataset.append(data)
-        
-    #     dataset = tf.data.Dataset.from_tensor_slices(dataset)
-
-    #     dataset = dataset.batch(self.batch_size)
-
-    #     if self.is_cache:
-    #         dataset = dataset.cache()
-
-    #     dataset = dataset.prefetch(self.AUTOTUNE)
-
-    #     return dataset
+        return math.ceil(len(self.data_paths) / self.batch_size)
     
     def __getitem__(self, index):
         if index in self.cache.keys():
@@ -134,11 +104,21 @@ class DataGenerator(Sequence):
         data = concate_data([data_0_batch, data_1_batch, data_2_batch, data_3_batch], axis=1)
 
         if self.is_lstm:
-            data = data.reshape(self.batch_size, 1, 4)
+            data = data.reshape(data.shape[0], 1, 4)
 
-        self.cache[index] = data, data
-        
-        return data, data
+        if self.train_mode == TrainMode.TEST.value:
+            self.cache[index] = data
+            return data
+        else:
+            self.cache[index] = data, data
+            return data, data
+
+    def on_epoch_end(self):
+        if self.is_predict == None:
+            datas = list(self.cache.values())
+            random.shuffle(datas)
+
+            self.cache = dict(zip(self.cache.keys(), datas))
 
     def adjust_data(self, data):        
         if self.is_adjust_fit:
@@ -147,4 +127,4 @@ class DataGenerator(Sequence):
         else:
             data = self.pipeline.transform(data)
 
-        return np.abs(data)
+        return abs(data)
