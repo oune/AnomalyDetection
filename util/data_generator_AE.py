77dca5dbe66f74efe3a68a93786911ca10f2404d
wowns1484 <wowns1484@naver.com>
2022-07-18 16:27:14 +0900

    [update] dataset class & process / [add] graph

--- util/data_generator_AE.py
+++ util/data_generator_AE.py
@@ -5,11 +5,11 @@ import enum
 import glob
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import Normalizer, MinMaxScaler, RobustScaler, StandardScaler
-from sklearn.pipeline import Pipeline
 from util.data_management import concate_data, stack_data
 import math
 import random
+from sklearn.preprocessing import Normalizer, MinMaxScaler, RobustScaler, StandardScaler
+from sklearn.pipeline import Pipeline
 
 class TrainMode(enum.Enum):
     TRAIN = 'Train'
@@ -17,54 +17,75 @@ class TrainMode(enum.Enum):
     TEST = 'Test'
 
 class DataGenerator(Sequence):
-    def __init__(self, dataset_path : str, train_mode : str, batch_size : int, split : tuple, is_train : bool, is_cache : bool, is_normalize : bool, is_lstm : bool, is_predict = None) -> None:
+    def __init__(self, dataset_path : str, train_mode : str, batch_size : int, split : tuple, is_cache : bool, is_normalize : bool, is_lstm : bool, is_adjust_fit : bool, pipeline = None, is_predict = None) -> None:
         super().__init__()
         self.AUTOTUNE = tf.data.experimental.AUTOTUNE
         self.dataset_path = dataset_path
         self.train_mode = train_mode
         self.batch_size = batch_size
         self.split = split
-        self.is_train = is_train
         self.is_cache = is_cache
         self.is_normalize = is_normalize
         self.is_lstm = is_lstm
-        self.is_adjust_fit = True
+        self.is_adjust_fit = is_adjust_fit
         self.is_predict = is_predict
         self.cache = {}
+        self.predict_labels = []
 
-        if self.is_normalize:
-            self.pipeline = Pipeline([('Normalizer' , Normalizer()), ('Scaler' , StandardScaler())])
+        if pipeline == None:
+            if self.is_normalize:
+                self.pipeline = Pipeline([('Scaler' , StandardScaler()), ('Normalizer' , Normalizer())])
+            else:
+                self.pipeline = Pipeline([('Scaler' , StandardScaler())])
         else:
-            self.pipeline = Pipeline([('Scaler' , StandardScaler())])
+            self.pipeline = pipeline
     
-        if is_train:
-            data_paths_0 = sorted(glob.glob(dataset_path + '\\Normal\\*_0_*.csv'))
-            data_paths_1 = sorted(glob.glob(dataset_path + '\\Normal\\*_1_*.csv'))
-            data_paths_2 = sorted(glob.glob(dataset_path + '\\Normal\\*_2_*.csv'))
-            data_paths_3 = sorted(glob.glob(dataset_path + '\\Normal\\*_3_*.csv'))
+        data_paths_0 = sorted(glob.glob(dataset_path + '\\Normal\\*_0_*.csv'))
+        data_paths_1 = sorted(glob.glob(dataset_path + '\\Normal\\*_1_*.csv'))
+        data_paths_2 = sorted(glob.glob(dataset_path + '\\Normal\\*_2_*.csv'))
+        data_paths_3 = sorted(glob.glob(dataset_path + '\\Normal\\*_3_*.csv'))
 
-            split_idx = int(len(data_paths_0)*self.split[0])
+        train_idx = int(len(data_paths_0)*self.split[0])
+        validation_idx = train_idx + int(int(len(data_paths_0)*self.split[1]))
 
-            if self.train_mode == TrainMode.TRAIN.value:
-                data_paths_0 = data_paths_0[:split_idx]
-                data_paths_1 = data_paths_1[:split_idx]
-                data_paths_2 = data_paths_2[:split_idx]
-                data_paths_3 = data_paths_3[:split_idx]
-            elif self.train_mode == TrainMode.VALIDATION.value:
-                data_paths_0 = data_paths_0[split_idx:]
-                data_paths_1 = data_paths_1[split_idx:]
-                data_paths_2 = data_paths_2[split_idx:]
-                data_paths_3 = data_paths_3[split_idx:]
+        if self.train_mode == TrainMode.TRAIN.value:
+            data_paths_0 = data_paths_0[:train_idx]
+            data_paths_1 = data_paths_1[:train_idx]
+            data_paths_2 = data_paths_2[:train_idx]
+            data_paths_3 = data_paths_3[:train_idx]
+        elif self.train_mode == TrainMode.VALIDATION.value:
+            data_paths_0 = data_paths_0[train_idx:validation_idx]
+            data_paths_1 = data_paths_1[train_idx:validation_idx]
+            data_paths_2 = data_paths_2[train_idx:validation_idx]
+            data_paths_3 = data_paths_3[train_idx:validation_idx]
+        elif self.train_mode == TrainMode.TEST.value:
+            data_paths_0 = data_paths_0[validation_idx:]
+            data_paths_1 = data_paths_1[validation_idx:]
+            data_paths_2 = data_paths_2[validation_idx:]
+            data_paths_3 = data_paths_3[validation_idx:]
 
-            self.data_paths = list(zip(data_paths_0, data_paths_1, data_paths_2, data_paths_3))
-        else:
-            data_paths_0 = sorted(glob.glob(dataset_path + '\\Abnormal\\*_0_*.csv'))
-            data_paths_1 = sorted(glob.glob(dataset_path + '\\Abnormal\\*_1_*.csv'))
-            data_paths_2 = sorted(glob.glob(dataset_path + '\\Abnormal\\*_2_*.csv'))
-            data_paths_3 = sorted(glob.glob(dataset_path + '\\Abnormal\\*_3_*.csv'))
+            data_paths_0 = [(data_path_0, [1.]) for data_path_0 in data_paths_0]
+            data_paths_1 = [(data_path_1, [1.]) for data_path_1 in data_paths_1]
+            data_paths_2 = [(data_path_2, [1.]) for data_path_2 in data_paths_2]
+            data_paths_3 = [(data_path_3, [1.]) for data_path_3 in data_paths_3]
 
-            self.data_paths = list(zip(data_paths_0, data_paths_1, data_paths_2, data_paths_3))
+            abnormals_0 = sorted(glob.glob(dataset_path + '\\Abnormal\\*_0_*.csv'))
+            abnormals_1 = sorted(glob.glob(dataset_path + '\\Abnormal\\*_1_*.csv'))
+            abnormals_2 = sorted(glob.glob(dataset_path + '\\Abnormal\\*_2_*.csv'))
+            abnormals_3 = sorted(glob.glob(dataset_path + '\\Abnormal\\*_3_*.csv'))
 
+            abnormals_0 = [(abnormal_0, [0.]) for abnormal_0 in abnormals_0]
+            abnormals_1 = [(abnormal_1, [0.]) for abnormal_1 in abnormals_1]
+            abnormals_2 = [(abnormal_2, [0.]) for abnormal_2 in abnormals_2]
+            abnormals_3 = [(abnormal_3, [0.]) for abnormal_3 in abnormals_3]
+
+            data_paths_0.extend(abnormals_0)
+            data_paths_1.extend(abnormals_1)
+            data_paths_2.extend(abnormals_2)
+            data_paths_3.extend(abnormals_3)
+       
+        self.data_paths = list(zip(data_paths_0, data_paths_1, data_paths_2, data_paths_3))
+        
     def __len__(self):
         return math.ceil(len(self.data_paths) / self.batch_size)
     
@@ -75,6 +96,12 @@ class DataGenerator(Sequence):
         dataset_batch = self.data_paths[index*self.batch_size:(index+1)*self.batch_size]
         data_0_batch, data_1_batch, data_2_batch, data_3_batch = [], [], [], []
         
+        if self.train_mode == TrainMode.TEST.value:
+            for data_0, _, _, _ in dataset_batch:
+                self.predict_labels.append(data_0[1])
+
+            dataset_batch = [(data[0][0], data[1][0], data[2][0], data[3][0]) for data in dataset_batch]
+
         for data_batch in dataset_batch:
             data_0 = pd.read_csv(data_batch[0], encoding='unicode_escape', delimiter='\t', header=None)
             data_1 = pd.read_csv(data_batch[1], encoding='unicode_escape', delimiter='\t', header=None)
